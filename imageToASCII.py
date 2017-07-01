c4b646f4aa7379a11c00d231a2af46f392dda906
unknown <ruisims@gmail.com>
2017-07-01 22:59:16 +0100

    Send project files to repo.

---
+++ imageToASCII.py
@@ -0,0 +1,26 @@
+from PIL import Image,ImageFilter
+
+a = open("image.html","w") 
+im = Image.open("sample.png")
+px = im.load()
+
+width,height = im.size
+
+print("<pre style=\"font: 10px/5px monospace; text-align: center;\">", file=a)
+for y in range(1, width):
+    for x in range(1, height):
+        pxRGB = im.getpixel((x,y))
+        R,G,B = pxRGB
+        brt = sum([R,G,B])/3/255
+        if brt>0 and brt <0.2:
+            print("#", end='', file=a)
+        elif brt >=0.2 and brt < 0.4:
+            print("O", end='', file=a)
+        elif brt >=0.4 and brt < 0.6:
+            print("|", end='', file=a)
+        elif brt >=0.6 and brt < 0.8:
+            print(",", end='', file=a)
+        else:
+            print(" ", end='', file=a)
+    print("", file=a)
+print("</pre>", file=a)
