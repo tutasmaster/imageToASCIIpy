83541f6d853648acd86516a3a854c1ca365a1cb3
unknown <ruisims@gmail.com>
2017-07-02 11:16:45 +0100

    Made it modular.

--- imageToASCII.py
+++ imageToASCII.py
@@ -1,9 +1,11 @@
-
 from PIL import Image
 
 a = open("image.html","w")
 
-im = Image.open("sample.png")
+symbols = [chr(219),"#","O","I","i",";","."," "]
+symbolSize = 1/len(symbols)
+
+im = Image.open("sample.png").convert('RGB')
 px = im.load()
 width,height = im.size
 
@@ -12,17 +14,9 @@ print("<pre style=\"font: 10px/5px monospace; text-align: center;\">", file=a)
 for y in range(0, height):
     for x in range(0, width):
         pxRGB = im.getpixel((x,y))
-        R,G,B,A = pxRGB
+        R,G,B = pxRGB
         brt = sum([R,G,B])/3/255
-        if brt>=0 and brt <0.2:
-            print("#", end='', file=a)
-        elif brt >=0.2 and brt < 0.4:
-            print("O", end='', file=a)
-        elif brt >=0.4 and brt < 0.6:
-            print("|", end='', file=a)
-        elif brt >=0.6 and brt < 0.8:
-            print(",", end='', file=a)
-        else:
-            print(" ", end='', file=a)
+        pos = round(brt/symbolSize)
+        print(symbols[pos - 1],end="",file=a)
     print("", file=a)
 print("</pre>", file=a)
